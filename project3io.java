8370aaa28a129da4a162dd99da5f4f0a20c45434
Rosaly Sia <RosalySia@users.noreply.github.com>
2017-01-26 06:23:12 -0800

    Rosaly
    PROJECT3io.java

---
+++ project3io.java
@@ -0,0 +1,12 @@
+
+class project3io{
+    public static void main(String[]args){
+        for(String j: args){
+            System.out.println("Hello " + j );
+        }
+        System.out.println("There are " + args.length + "Stranger");
+        System.out.println("Hello Everyone!");
+        System.out.println("I'm Rosaly B. Sia :)");
+        System.out.println("Have a good day!");
+    }
+}
